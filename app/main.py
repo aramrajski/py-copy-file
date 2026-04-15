2bdb0b0068922430359ea576a9abc8fc6e312a12
aramrajski <aramrajski@gmail.com>
2026-04-15 15:47:49 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import os
 
 def copy_file(command: str) -> None:
     parts = command.split()
-    src = parts[1]
-    dst = parts[2]
     if len(parts) == 3 and parts[0] == "cp":
+        src = parts[1]
+        dst = parts[2]
         if src == dst or not os.path.exists(src):
             return
         with (open(f"{src}", "r") as org, open(f"{dst}", "w") as new):
